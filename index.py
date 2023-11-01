94e4ec581962888ad654b27d97b6c8fe6df6d696
Bhairav1234 <113030793+Bhairav1234@users.noreply.github.com>
2023-11-01 14:54:52 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+
+string1 = "GeeksforGeeks"
+
+string2 = "Geeks for geeks"
+
+if (string1 is string2):
+	print("Strings Are Equal")
+else:
+	print("Strings Are Not Equal")
+
